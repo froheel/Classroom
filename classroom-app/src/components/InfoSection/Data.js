7211a6f357dcef6d41d0fcbd39a6641f772813b5
froheel <fezaroheel2124@gmail.com>
2021-05-31 23:06:03 +0500

    Added the about page

---
+++ classroom-app/src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'What are you waiting for?',
+    headline: 'Join Today and Learn',
+    description: 'Get access to our exclusive app that allows you to join multiple classes taught by professional teachers',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: false,
+    primary: true,
+    darkText: false,
+
+}
--- classroom-app/src/pages/index.js
+++ classroom-app/src/pages/index.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import HeroSection from '../components/HeroSection'
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne } from '../components/InfoSection/Data';
 const Home = () => {
 
     const[isOpen, setIsOpen] = useState(false)
@@ -15,6 +17,7 @@ const Home = () => {
         <Sidebar isOpen = {isOpen} toggle={toggle}></Sidebar>
         <Navbar toggle={toggle}></Navbar>
         <HeroSection></HeroSection>
+        <InfoSection{...homeObjOne}></InfoSection>
         </>
     )
 }
