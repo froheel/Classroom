83699aa13d9a5c8a8d4526eb995c8cb2a7117ace
froheel <fezaroheel2124@gmail.com>
2021-05-31 23:45:36 +0500

    Added pages for disover

--- classroom-app/src/components/InfoSection/Data.js
+++ classroom-app/src/components/InfoSection/Data.js
@@ -5,12 +5,48 @@ export const homeObjOne = {
     lightTextDesc: true,
     topLine: 'What are you waiting for?',
     headline: 'Join Today and Learn',
-    description: 'Get access to our exclusive app that allows you to join multiple classes taught by professional teachers',
+    description: 'Get access to our exclusive app that allows you to join multiple classes taught by professional teachers.',
     buttonLabel: 'Get started',
     imgStart: false,
     img: require('../../images/svg-1.svg').default,
     alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false,
+
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Confused where to start?',
+    headline: 'We got you. Just sign up for our application',
+    description: 'Our easy to use interface will make learning even more easier and fun.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'book',
     dark: false,
+    primary: false,
+    darkText: true,
+
+}
+
+export const homeObjThree = {
+    id: 'services',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Unable to understand complex concepts?',
+    headline: 'Learn from Professional Teachers',
+    description: 'Certified and knowledgeable teachers will teach classes. Each concept will not just be explained with text but also with graphs, charts and diagrams.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/svg-3.svg').default,
+    alt: 'Notebook',
+    dark: true,
     primary: true,
     darkText: false,
 
