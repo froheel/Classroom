7b482fbef02b62af1eed54f7e143932d91605570
froheel <fezaroheel2124@gmail.com>
2021-06-01 23:55:25 +0500

    Added classes at /classes and when clicked on icons redirects to homepage(should be redirected to class)

--- classroom-app/src/App.js
+++ classroom-app/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './pages';
 import SignInPage from './pages/signin';
 import SignUpPage from './pages/signup';
+import ClassesPage from './pages/classes';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" component={Home} exact />
         <Route path = "/signin" component={SignInPage} exact/>
         <Route path = "/signup" component = {SignUpPage} exact/>
+        <Route path= "/classes" component ={ClassesPage} exact/>
       </Switch>
     </Router>
   );
