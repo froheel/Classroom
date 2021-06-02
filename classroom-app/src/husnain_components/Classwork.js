9c2e170d68a7e686991244df3692693cee185db1
froheel <fezaroheel2124@gmail.com>
2021-06-02 15:14:01 +0500

    Added dummy data in Assignment, Project & Homeworks

--- classroom-app/src/husnain_components/Classwork.js
+++ classroom-app/src/husnain_components/Classwork.js
@@ -10,24 +10,24 @@ class Classwork extends Component {
                          style={{marginTop: 30, marginBottom: 30}}>
                         <h1 className={"text-primary"}>Homework</h1>
                     </div>
-                    <ClassworkCard name="Assignment 1" description="
+                    <ClassworkCard name="Homework 3" description="
 In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available"/>
                     <div className={"block-example border-top border-dark"}></div>
 
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Homework 2" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
 
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Homework 1" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
 
                     <div className={"block-example border-bottom border-dark"}
                          style={{marginTop: 30, marginBottom: 30}}>
                         <h1 className={"text-primary"}>Assignment</h1>
                     </div>
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Assignment 3" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
 
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Assignment 2" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
 
                     <ClassworkCard name="Assignment 1" description="this is a1"/>
@@ -36,13 +36,13 @@ In publishing and graphic design, Lorem ipsum is a placeholder text commonly use
                          style={{marginTop: 30, marginBottom: 30}}>
                         <h1 className={"text-primary"}>Project</h1>
                     </div>
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Project 3" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
 
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Project 2" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
 
-                    <ClassworkCard name="Assignment 1" description="this is a1"/>
+                    <ClassworkCard name="Project 1" description="this is a1"/>
                     <div className={"block-example border-top border-dark"}></div>
                 </div>
             </div>
