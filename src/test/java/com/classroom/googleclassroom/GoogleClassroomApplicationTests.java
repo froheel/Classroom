6e5a8521bea8f8bf1f0a324600426c82331df686
husnain7766 <40701771+husnain7766@users.noreply.github.com>
2021-06-12 10:44:36 +0500

    First commit

---
+++ src/test/java/com/classroom/googleclassroom/GoogleClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classroom.googleclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleClassroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
